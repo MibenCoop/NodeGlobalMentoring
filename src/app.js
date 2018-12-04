613a00177b67f02c9fd4fbe0a446793280347909
MibenCoop <vitalii.belmach@clarivate.com>
2018-12-04 18:40:58 +0300

    Added indexjs and Eslinter

--- src/app.js
+++ src/app.js
@@ -1,9 +1,5 @@
-import appConfig from '../config/config.json'
-//Task was one import command that bring all modelt to the upp
-import {User, Product } from '../models/modelsProvider'
-//But as for me it's better solution:
-// import User from '../models/User'
-// import Product from '../models/Product'
+import appConfig from "../config/config.json";
+import { User, Product } from "../models";
 
 console.log(appConfig.name);
 let user = new User();
