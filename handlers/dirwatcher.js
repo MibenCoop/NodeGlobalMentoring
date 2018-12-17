1e5f261d36bfb61c6cc6640e1e0e39e2717b39ea
MibenCoop <vitalii.belmach@clarivate.com>
2018-12-17 16:30:40 +0300

    added DI

--- handlers/dirwatcher.js
+++ handlers/dirwatcher.js
@@ -1,24 +1,42 @@
 import * as colors from "colors";
 import * as fs from "fs";
-import { default as dataEmitter } from "./data_emitter";
 
+const WATCHER_INITIALIZED_LOG = "Initialize step: DirWatcher was initialized";
+const SEPARATOR = "____________________________Next Interval____________________________";
+const STOP_WATCHING = "Stop watching";
 export default class DirWatcher {
-	constructor() {
-		console.log("Initialize step: DirWatcher was initialized".grey);
+	constructor(dataEmitter) {
+		console.log(WATCHER_INITIALIZED_LOG.grey);
+		this.dataEmitter = dataEmitter;
+		this.timer = null;
+		this.downloadedFiles = [];
 	}
+
 	watch(path, delay) {
-		let downloadedFiles = [];
-		setInterval(() => {
-			console.log("____________________________Next Interval____________________________");
-			fs.readdir(path, (files) => {
-				files.filter((file) => {
-					if ( file.match(/\.csv/g) !== null && downloadedFiles.indexOf(file) === -1 ) {
-						dataEmitter.emit("changed", path+file);
-						downloadedFiles.push(file);
-						console.log("Downloaded Files", downloadedFiles);
-					} 
+		this.timer = setInterval(() => {
+			console.log(SEPARATOR);
+			fs.readdir(path, (err, files) => {
+				files.forEach(file => {
+					this.sendFileEvent(path, file);
 				});
 			});
 		}, delay);
+	}
+
+	checkCSVFile(file) {
+		return file.match(/\.csv/g) !== null && this.downloadedFiles.indexOf(file) === -1 ?  true :  false;
+	}
+
+	sendFileEvent(path, file) {
+		if ( this.checkCSVFile(file) ) {
+			this.dataEmitter.emit("changed", path+file);
+			this.downloadedFiles.push(file);
+			console.log(`Downloaded Files: ${this.downloadedFiles}`);
+		} 
+	}
+	
+	stopWatching() {
+		console.log(STOP_WATCHING);
+		clearInterval(this.timer);
 	}
 }
