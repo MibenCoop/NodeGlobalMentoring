ddbcc41c03f439d9fa79378b4acef2fb2a4f1779
MibenCoop <vitalii.belmach@clarivate.com>
2019-02-08 17:55:44 +0300

    Completed task6

--- express-server/index.js
+++ express-server/index.js
@@ -1,3 +1,12 @@
-import {app} from './app';
+import app from './app';
+import cookieParser from 'cookie-parser';
+
 const port = process.env.PORT || 3100;
+app.use(cookieParser());
+app.get('/', function(req, res) {
+    console.log('Cookies: ', req.cookies);
+    if (Object.keys(req.cookies).length !== 0 && (req.query).constructor === Object) {
+        req.params.parsedCookies = req.cookies;
+    }
+});
 app.listen(port, () => console.log(`App listening on port ${port}!`))
