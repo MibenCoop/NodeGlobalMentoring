fdd02465ba1146d5b0c844f2187d6659586e5bc2
MibenCoop <vitalii.belmach@clarivate.com>
2019-02-08 18:46:00 +0300

    Init task 7

--- express-server/index.js
+++ express-server/index.js
@@ -1,8 +1,18 @@
 import app from './app';
 import cookieParser from 'cookie-parser';
+import queryString from 'query-string';
 
 const port = process.env.PORT || 3100;
 app.use(cookieParser());
+app.use(function (req, res, next) {
+    console.log('Query: ', req.query);
+    if (Object.keys(req.query).length !== 0 && (req.query).constructor === Object) {
+        req.params = {
+            parsedQuery: req.query
+        };
+    }
+    next();
+});
 app.get('/', function(req, res) {
     console.log('Cookies: ', req.cookies);
     if (Object.keys(req.cookies).length !== 0 && (req.query).constructor === Object) {
