1b0ec615e56908b353f2fbd2747e288ff4fc6d1e
MibenCoop <vitalii.belmach@clarivate.com>
2019-02-14 18:09:51 +0300

    task3

--- express-server/src/resourses/products/router.js
+++ express-server/src/resourses/products/router.js
@@ -1,32 +1,29 @@
 import express from 'express';
 const router = express.Router();
-import productsJSON from '../../models/products';
-
+import products from '../../models/products';
+import {checkToken} from '../../common/utils';
 //Get all products
-router.get('/', (req, res) => {
-    const parsedProducts = JSON.stringify(productsJSON);
-    res.send(parsedProducts);
+router.get('/', checkToken, (req, res) => {
+    res.send(products);
 });
 
 //Get by Id
-router.get('/:id', (req, res) => {
-    let product = productsJSON.products.filter(elem => elem.id === Number(req.params.id));
+router.get('/:id', checkToken, (req, res) => {
+    let product = products.filter(elem => elem.id === Number(req.params.id));
                                     //spread array to object
-    const parsedProducts = JSON.stringify(...product);
-    res.send(parsedProducts);
+    res.send(product);
 });
 
 //Get product reviews
-router.get('/:id/reviews', (req, res) => {
-    let product = productsJSON.products.filter(elem => elem.id === Number(req.params.id));
-    const parsedProducts = JSON.stringify(product[0].reviews);
-    res.send(parsedProducts);
+router.get('/:id/reviews', checkToken, (req, res) => {
+    let product = products.filter(elem => elem.id === Number(req.params.id));
+    res.send(product[0].reviews);
 });
 
 
-//Not very convenenient to implementing POST without DB
-router.post('/products', (req, res) => {
-    //Save to DB
-});
+// //Not very convenenient to implementing POST without DB
+// router.post('/products', checkToken, (req, res) => {
+//     //Save to DB
+// });
 
 export default router;
